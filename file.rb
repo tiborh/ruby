5a657b99c144ff2e58a47c3e4a3da34528ffdfc7
Tibor Harcsa <silurust@gmail.com>
2017-09-10 01:09:08 +0200

    explanations

--- file.rb
+++ file.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 
+# __FILE__ gives the filename of this file
 puts __FILE__
 puts __FILE__.split("/").last
+# $0 and $PROGRAM_NAME give the name of the file executed
 puts $0
 puts $PROGRAM_NAME
+# so if a module or class is used in a different file (with require or require_relative,
+# __FILE__ used in the required file will give the name of that file
+# $0 will show the name of the file originally executed, that is, the requirer
