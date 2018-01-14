3f8c53e9284cc847e98f30ca34843e26b677bf82
Tibor Harcsa <tibor@harcsa.net>
2018-01-14 01:30:23 +0100

    tutorial from https://github.com/gosu/gosu/wiki/Ruby-Tutorial started

---
+++ gosu/tutorial/empty.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'gosu'
+
+class Tutorial < Gosu::Window
+  def initialize
+    super 640, 480
+    self.caption = "Tutorial Game"
+  end
+  
+  def update
+    # ...
+  end
+  
+  def draw
+    # ...
+  end
+end
+
+Tutorial.new.show
