6236d220ddcf7153a0256e453995dde3a31412f4
tibor <silurust@gmail.com>
2018-05-14 02:35:23 +0200

    some more file operations

--- file.rb
+++ file.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
 # __FILE__ gives the filename of this file
-puts __FILE__
-puts __FILE__.split("/").last
+puts "__FILE__", __FILE__
+puts "__FILE__.split(\"/\").last", __FILE__.split("/").last
 # $0 and $PROGRAM_NAME give the name of the file executed
-puts $0
-puts $PROGRAM_NAME
+puts "$0",$0
+puts "$PROGRAM_NAME",$PROGRAM_NAME
 # so if a module or class is used in a different file (with require or require_relative,
 # __FILE__ used in the required file will give the name of that file
 # $0 will show the name of the file originally executed, that is, the requirer
+
+require_relative "file_module"
+include FILEMOD
+write_file_stuff()
